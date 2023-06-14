c92b5ac4f667c99179491574516f540c7a90293d
Hezron-tech <kiprophez@gmail.com>
2023-06-14 23:51:37 +0300

    simple calc

--- Program.cs
+++ Program.cs
@@ -14,14 +14,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             //DATATYPES
 
-            string phrase="Hezron academy";
+            // string phrase="Hezron academy";
             // char grade='A';
             // int age =30;
             // double gpa = 3.2;
             // bool isMale = true;
 
-            Console.WriteLine("Hezron\n Academy");
-            Console.WriteLine(phrase);
+            // Console.WriteLine("Hezron\n Academy");
+            // Console.WriteLine(phrase);
+            // Console.WriteLine(phrase[5]);
+            //  Console.WriteLine(phrase.ToUpper());
+
+            Console.Write("Enter your name:");
+           
+            string name = Console.ReadLine();
+            Console.Write("Enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello  " +name  +   "your age is" + age);
+            Console.ReadLine();
            
         }
     }
